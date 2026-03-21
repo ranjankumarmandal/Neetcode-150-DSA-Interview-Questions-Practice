2945809a53188411ec5cec801c115cef8a8b5b69
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-21 17:07:22 +0530

    Neetcode 150 DSA Practice: array & hashing: valid anagram problem: completed

--- src/main/java/org/example/a_array_and_hashing/ValidAnagram.java
+++ src/main/java/org/example/a_array_and_hashing/ValidAnagram.java
@@ -2,6 +2,21 @@ package org.example.a_array_and_hashing;
 
 public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
 
+        int[] counts = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+
+        for(int count : counts) {
+            if(count != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
