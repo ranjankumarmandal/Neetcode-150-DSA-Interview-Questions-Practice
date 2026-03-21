0bfb6f6c03d686f8f6e7006cfd7252b2e44c779c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-21 17:18:16 +0530

    Neetcode 150 DSA Practice: array & hashing: two sum: completed

--- src/main/java/org/example/a_array_and_hashing/TwoSum.java
+++ src/main/java/org/example/a_array_and_hashing/TwoSum.java
@@ -1,7 +1,21 @@
 package org.example.a_array_and_hashing;
 
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return null;
     }
 }
