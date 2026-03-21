1fc8c26e25bc6abe40414ea00ec05e855c2b8fed
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-21 17:34:33 +0530

    Neetcode 150 DSA Practice: array & hashing: group anagram: completed

--- src/main/java/org/example/a_array_and_hashing/GroupAnagrams.java
+++ src/main/java/org/example/a_array_and_hashing/GroupAnagrams.java
@@ -1,9 +1,25 @@
 package org.example.a_array_and_hashing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
 
+        for(String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
