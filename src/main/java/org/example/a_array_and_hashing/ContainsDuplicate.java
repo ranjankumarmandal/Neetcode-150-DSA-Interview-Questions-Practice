902ff082e87355f473a2d9274961a247b2e83bfd
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-21 13:34:47 +0530

    Neetcode 150 DSA Practice: array & hashing: contains duplicate problem: completed

--- src/main/java/org/example/a_array_and_hashing/ContainsDuplicate.java
+++ src/main/java/org/example/a_array_and_hashing/ContainsDuplicate.java
@@ -1,4 +1,22 @@
 package org.example.a_array_and_hashing;
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+        // a hashset
+        // iterate through each element in array
+         // if set contains element - if yes - return true;
+         // else set.add(arr[i])
+        // return false;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
 }
