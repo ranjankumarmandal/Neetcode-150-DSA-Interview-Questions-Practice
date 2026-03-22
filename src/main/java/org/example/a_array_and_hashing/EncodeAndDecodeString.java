2c1873889826a8776520ee9fd0b82548bd2a1b26
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-22 12:59:30 +0530

    Neetcode 150 DSA Practice: array & hashing: encode and decode string: started

--- src/main/java/org/example/a_array_and_hashing/EncodeAndDecodeString.java
+++ src/main/java/org/example/a_array_and_hashing/EncodeAndDecodeString.java
@@ -1,4 +1,13 @@
 package org.example.a_array_and_hashing;
 
+import java.util.List;
+
 public class EncodeAndDecodeString {
+    public String encode(List<String> strs) {
+
+    }
+
+    public List<String> decode(String str) {
+
+    }
 }
