189b34dfc0cff81534c9d50d97c8ef642e2c6463
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-22 13:00:18 +0530

    Neetcode 150 DSA Practice: array & hashing: encode and decode string: completed

--- src/main/java/org/example/a_array_and_hashing/EncodeAndDecodeString.java
+++ src/main/java/org/example/a_array_and_hashing/EncodeAndDecodeString.java
@@ -4,10 +4,37 @@ import java.util.List;
 
 public class EncodeAndDecodeString {
     public String encode(List<String> strs) {
+        StringBuilder encoded = new StringBuilder();
 
+        for(String s : strs) {
+            encoded.append(s.length());
+            encoded.append(':');
+            encoded.append(s);
+        }
+
+        return encoded.toString();
     }
 
     public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
 
+        while(i < str.length()) {
+            int j = i;
+            while(str.charAt(j) != ':') {
+                j++;
+            }
+
+            int len = Integer.parseInt(str.substring(i, j));
+            j++;
+
+            String s = str.substring(i, j + len);
+
+            result.add(s);
+
+            i = j + len;
+        }
+
+        return result;
     }
 }
