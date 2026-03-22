38c765ddd2dd227a8f466a2d15efc5095f0e66a9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-22 13:21:58 +0530

    Neetcode 150 DSA Practice: array & hashing: products of array except self: completed

--- src/main/java/org/example/a_array_and_hashing/ProductOfArrayExceptSelf.java
+++ src/main/java/org/example/a_array_and_hashing/ProductOfArrayExceptSelf.java
@@ -2,6 +2,25 @@ package org.example.a_array_and_hashing;
 
 public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
 
+        for(int i = 0; i < n; i++) {
+            output[i] = 1;
+        }
+
+        int prefix = 1;
+        for(int i = 0; i < n; i++) {
+            output[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            output[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return output;
     }
 }
