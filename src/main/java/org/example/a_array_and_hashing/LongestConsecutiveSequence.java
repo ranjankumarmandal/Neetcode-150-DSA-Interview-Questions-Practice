e740deb8154c7ec576e0ce9abb066ed25e7fe3af
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-22 14:04:04 +0530

    Neetcode 150 DSA Practice: array & hashing: longest consecutive sequence: completed

--- src/main/java/org/example/a_array_and_hashing/LongestConsecutiveSequence.java
+++ src/main/java/org/example/a_array_and_hashing/LongestConsecutiveSequence.java
@@ -1,7 +1,30 @@
 package org.example.a_array_and_hashing;
 
+import java.util.HashSet;
+
 public class LongestConsecutiveSequence {
     public int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for(int num : nums) {
+            numSet.add(num);
+        }
 
+        int maxLen = 0;
+
+        for(int num : nums) {
+            if(!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentLen = 1;
+
+                while(numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentLen += 1;
+                }
+
+                if(currentLen > maxLen) maxLen = currentLen;
+            }
+        }
+
+        return maxLen;
     }
 }
