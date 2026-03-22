3a45770625074724d008da7d217e3cf973776ccd
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-22 13:46:45 +0530

    Neetcode 150 DSA Practice: array & hashing: valid sukoku: completed: bugfix

--- src/main/java/org/example/a_array_and_hashing/ValidSudoku.java
+++ src/main/java/org/example/a_array_and_hashing/ValidSudoku.java
@@ -4,9 +4,9 @@ import java.util.HashSet;
 
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character> rows = new HashSet[9];
-        HashSet<Character> cols = new HashSet[9];
-        HashSet<Character> boxes = new HashSet[9];
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
 
         for(int i = 0; i < 9; i++) {
             rows[i] = new HashSet<>();
