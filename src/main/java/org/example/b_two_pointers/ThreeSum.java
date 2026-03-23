45430c88faee3623f5a1f096b17089da4613b20a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-23 12:56:56 +0530

    Neetcode 150 DSA Practice: three sum: completed

--- src/main/java/org/example/b_two_pointers/ThreeSum.java
+++ src/main/java/org/example/b_two_pointers/ThreeSum.java
@@ -4,6 +4,35 @@ import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum < 0) {
+                    left++;
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[left], nums[right]);
+                    result.add(triplet);
+
+                    while(left < right && nums[left] == nums[left - 1])l left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
     }
 }
