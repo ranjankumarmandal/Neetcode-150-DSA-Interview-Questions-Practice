2726e1e54fdcaddaa8a76f800616915f7863f4ff
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-23 22:22:25 +0530

    Neetcode 150 DSA Practice: sliding window: Longest Substring Without Repeating Characters: completed

--- src/main/java/org/example/c_sliding_window/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/org/example/c_sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,27 @@
 package org.example.c_sliding_window;
 
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> seen = new HashSet<>();
 
+        int left = 0, maxLen = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+            char c  = s.charAt(right);
+
+            while(seen.contains(c)) {
+                seen.remove(s.charAt(left));
+                left++;
+            }
+
+            seen.add(c);
+
+            int currLen = right - left + 1;
+            if(currLen > maxLen) maxLen = currLen;
+        }
+
+        return maxLen;
     }
 }
