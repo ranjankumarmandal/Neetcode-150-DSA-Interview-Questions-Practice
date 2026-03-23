818acb567f604ec5dc7449c79b3e59989f88a878
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-23 11:15:20 +0530

    Neetcode 150 DSA Practice: two pointers: valid palindrome: completed

--- src/main/java/org/example/b_two_pointers/ValidPalindrome.java
+++ src/main/java/org/example/b_two_pointers/ValidPalindrome.java
@@ -2,6 +2,25 @@ package org.example.b_two_pointers;
 
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
 
+        while(left < right) {
+            while(left < right && !Character.isLetterOrDigit(left)) {
+                left++;
+            }
+
+            while(left < right && !Character.isLetterOrDigit(right)) {
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
