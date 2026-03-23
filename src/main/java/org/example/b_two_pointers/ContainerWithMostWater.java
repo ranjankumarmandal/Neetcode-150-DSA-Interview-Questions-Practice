5e4a1ee4110843b5684de8f71502b7e69e685780
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-23 13:17:00 +0530

    Neetcode 150 DSA Practice: container with most water: completed

--- src/main/java/org/example/b_two_pointers/ContainerWithMostWater.java
+++ src/main/java/org/example/b_two_pointers/ContainerWithMostWater.java
@@ -2,6 +2,21 @@ package org.example.b_two_pointers;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] heights) {
+        int maxArea = 0;
+        int left = 0, right = heights.length - 1;
 
+        while(left < right) {
+            int currentHeight = Math.min(heights[left], heights[right]);
+            int width = right - left;
+
+            int area = currentHeight * width;
+
+            if(area > maxArea) maxArea = area;
+
+            if(heights[left] < heights[right]) left++;
+            else right--;
+        }
+
+        return maxArea;
     }
 }
