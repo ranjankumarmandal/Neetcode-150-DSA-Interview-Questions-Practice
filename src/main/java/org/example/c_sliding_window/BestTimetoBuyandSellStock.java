d10bfc78bed0919246770b8fdd7027e7e72bdcf1
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-23 22:05:14 +0530

    Neetcode 150 DSA Practice: sliding window: best time to buy and sell stock: completed

--- src/main/java/org/example/c_sliding_window/BestTimetoBuyandSellStock.java
+++ src/main/java/org/example/c_sliding_window/BestTimetoBuyandSellStock.java
@@ -2,6 +2,18 @@ package org.example.c_sliding_window;
 
 public class BestTimetoBuyandSellStock {
     public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
 
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < minPrice) minPrice = prices[i];
+            else {
+                int profit = prices[i] - minPrice;
+
+                if(profit > maxProfit) maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
     }
 }
