fe38bdd5d20bfc26a2080114fdb715cb5098f8a8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-25 23:15:20 +0530

    Neetcode 150 DSA Practice: stack: CarFleet: completed

--- d_stack/CarFleet.java
+++ d_stack/CarFleet.java
@@ -1,5 +1,28 @@
 public class CarFleet {
     public int carFleet(int target, int[] position, int[] speed) {
+        int n = posotion.length;
+        if(n == 0) return return 0;
 
+        double[][] cars = new double[n][2];
+        for(int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        for(int i = 0; i < n; i++) {
+            double time = cars[i][1];
+
+            if(time > lastTime) {
+                fleets++;
+                lastTime = time;
+            }
+        }
+
+        return fleets;
     }
 }
