85cc41d34b48c9737cb6c6e40b5ef766ce891b4b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-25 20:30:39 +0530

    Neetcode 150 DSA Practice: stack: Min Stack: started

--- src/main/java/org/example/d_stack/MinStack.java
+++ src/main/java/org/example/d_stack/MinStack.java
@@ -1,4 +1,23 @@
 package org.example.d_stack;
 
 public class MinStack {
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
 }
