a303cd60044ece2adb58c82a20c19183b8ca8dac
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-25 20:44:52 +0530

    Neetcode 150 DSA Practice: stack: Min Stack: completed

--- src/main/java/org/example/d_stack/MinStack.java
+++ src/main/java/org/example/d_stack/MinStack.java
@@ -1,23 +1,40 @@
 package org.example.d_stack;
 
-public class MinStack {
-    public MinStack() {
+import java.util.Stack;
 
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int val) {
+        stack.push(val);
 
+        if(minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
     }
 
     public void pop() {
-
+        if(!stack.isEmpty()) {
+            stack.pop();
+            minStack.pop();
+        }
     }
 
     public int top() {
-
+        if(stack.isEmpty()) return -1;
+        return stack.peek();
     }
 
     public int getMin() {
-
+        if(minStack.isEmpty()) return -1;
+        return minStack.peek();
     }
 }
