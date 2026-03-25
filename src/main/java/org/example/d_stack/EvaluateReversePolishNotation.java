2d20321b5cbed4b76801dba13707e830acfe6e5a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-25 22:07:29 +0530

    Neetcode 150 DSA Practice: stack: Evaluate Reverse Polish Notation: completed

--- src/main/java/org/example/d_stack/EvaluateReversePolishNotation.java
+++ src/main/java/org/example/d_stack/EvaluateReversePolishNotation.java
@@ -1,7 +1,30 @@
 package org.example.d_stack;
 
+import java.util.Stack;
+
 public class EvaluateReversePolishNotation {
     public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
 
+        for(int i = 0; i < tokens.length; i++) {
+            String t = tokens[i];
+
+            if(t.equals("+") || t.equals("-") || t.equals("*") || t.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = 0;
+
+                if(t.equals("+")) result = a + b;
+                else if(t.equals("-")) result = a - b;
+                else if(t.equals("*")) result = a * b;
+                else result = a / b;
+
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(t));
+            }
+        }
+
+        return stack.pop();
     }
 }
