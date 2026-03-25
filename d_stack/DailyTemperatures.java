e290a64c9d7d97e51ac09d830fe92a4e911b376f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-25 22:52:27 +0530

    Neetcode 150 DSA Practice: stack: Daily Temperatures: completed

--- d_stack/DailyTemperatures.java
+++ d_stack/DailyTemperatures.java
@@ -1,5 +1,17 @@
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
 
+        for(i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
     }
 }
