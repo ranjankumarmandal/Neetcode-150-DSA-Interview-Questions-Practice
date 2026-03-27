e8f317c066f170269331f3d862743a9992176b33
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-27 10:28:50 +0530

    Neetcode 150 DSA Practice: Binary Search: Search a 2D Matrix: completed

--- e_binarysearch/SearchIn2DMatrix.java
+++ e_binarysearch/SearchIn2DMatrix.java
@@ -1,5 +1,29 @@
 public class SearchIn2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
 
+        if(matrix[0].length == 0) return false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = 0;
+
+        while(left <= right) {
+            int mid = (left + rght) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int midValue = matrix[row][col];
+
+            if(midValue == target) {
+                return true;
+            } else if(midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
     }
 }
