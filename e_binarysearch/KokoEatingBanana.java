6ebaf61a5ff794408a2fd14c49c1f7899e38d0bf
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-27 10:43:43 +0530

    Neetcode 150 DSA Practice: Binary Search: Koko Eating Bananas: completed

--- e_binarysearch/KokoEatingBanana.java
+++ e_binarysearch/KokoEatingBanana.java
@@ -1,5 +1,27 @@
 public class KokoEatingBanana {
     public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for(int p : piles) {
+            if(p > max) max = p;
+        }
 
+        int left = 1, right = max;
+
+        while(left < right) {
+            int mid (left + right) / 2;
+            int totalHours = 0;
+
+            for(int p : piles) {
+                totalHours = (p + mid - 1) / mid;
+            }
+
+            if(totalHours <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
