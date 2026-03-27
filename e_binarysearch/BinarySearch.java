2fecccde51cf6b6fe650e5695b56b9ebc7fbeea0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-27 10:15:11 +0530

    Neetcode 150 DSA Practice: Binary Search: Binary Search: completed

--- e_binarysearch/BinarySearch.java
+++ e_binarysearch/BinarySearch.java
@@ -1,5 +1,19 @@
 public class BinarySearch {
     public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
 
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
