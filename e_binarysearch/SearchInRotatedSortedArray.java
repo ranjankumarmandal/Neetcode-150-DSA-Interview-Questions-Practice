cfacb7291bf3eaf283eecf4423b9fce2618f0d2e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-28 18:30:15 +0530

    Neetcode 150 DSA Practice: Binary Search: Search in Rotated Sorted Array: in-progress

--- e_binarysearch/SearchInRotatedSortedArray.java
+++ e_binarysearch/SearchInRotatedSortedArray.java
@@ -1,5 +1,14 @@
 public  class SearchInRotatedSortedArray {
     public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target) return mid;
+        }
+
+        reutrn -1;
     }
 }
