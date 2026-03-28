2de8df9a6f05768c57897a5298084882ce7610c3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-28 18:52:00 +0530

    Neetcode 150 DSA Practice: Binary Search: Time Based Key-Value Store: completed

--- e_binarysearch/TimeBasedKeyValueStore.java
+++ e_binarysearch/TimeBasedKeyValueStore.java
@@ -19,5 +19,25 @@ public class TimeBasedKeyValueStore {
         map.get(key).add(new Pair(timestamp, value));
     }
 
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)) return "";
 
+        List<Pair> pairs = map.get(key);
+        int left = 0, right = pairs.size() - 1;
+        String result = "";
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(pairs.get(mid).timestamp = timestamp) return pairs.get(mid).value;
+            else if(pairs.get(mid).timestamp < timestamp) {
+                result = pairs.get(mid).value;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
 }
