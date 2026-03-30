d574eda9c91358b3acc4fa6de938e322c0c0c718
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-30 13:34:12 +0530

    Neetcode 150 DSA Practice: Linked List: Linked List Cycle Detection: completed

--- f_linkedlist/LinkedListCycleDetection.java
+++ f_linkedlist/LinkedListCycleDetection.java
@@ -1,5 +1,14 @@
 public class LinkedListCycleDetection {
     public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<>();
+        ListNode curr = head;
 
+        while(curr != null) {
+            if(seen.contains(curr)) return true;
+            seen.add(curr);
+            curr = curr.next;
+        }
+
+        return false;
     }
 }
