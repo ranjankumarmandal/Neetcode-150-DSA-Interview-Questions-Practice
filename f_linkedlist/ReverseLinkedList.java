6f6d920317dda216d10a4d1cc079c67ae5903df9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-30 11:27:34 +0530

    Neetcode 150 DSA Practice: Linked List: Reverse Linked List: in-progress v2

--- f_linkedlist/ReverseLinkedList.java
+++ f_linkedlist/ReverseLinkedList.java
@@ -2,7 +2,14 @@ public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         ListNode curr = head, prev = null, next;
 
+        while(curr != null) {
+            next = curr.next;
 
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
 
     }
 }
