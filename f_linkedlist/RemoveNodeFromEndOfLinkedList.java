60d3859300185041120d35b581b7a4d95b8ee4d9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-31 21:43:16 +0530

    Neetcode 150 DSA Practice: Linked List: Remove Node From End of Linked List: in-progress v2

--- f_linkedlist/RemoveNodeFromEndOfLinkedList.java
+++ f_linkedlist/RemoveNodeFromEndOfLinkedList.java
@@ -2,7 +2,10 @@ public class RemoveNodeFromEndOfLinkedList {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int length = 0;
         ListNode curr = head;
-
+        while (curr != null) {
+            length++;
+            curr = curr.next;
+        }
 
         return head;
     }
