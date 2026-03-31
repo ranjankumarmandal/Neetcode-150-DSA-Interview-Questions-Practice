c6ababbf115713d5d4b3a35b16119c04829559e5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-31 21:36:17 +0530

    Neetcode 150 DSA Practice: Linked List: Reorder Linked List: in-progress v1

--- f_linkedlist/ReorderLinkedList.java
+++ f_linkedlist/ReorderLinkedList.java
@@ -1,5 +1,34 @@
 public class ReorderLinkedList {
     public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        if (head.next == null) {
+            return;
+        }
 
+        int left = 0;
+        int right = nodes.size() - 1;
+
+        ListNode tail = null;
+
+        while (left <= right) {
+            if (tail == null) {
+                tail = nodes.get(left);
+            } else {
+                tail.next = nodes.get(left);
+            }
+            left = left + 1;
+
+            if (left > right) {
+                break;
+            }
+
+            tail.next = nodes.get(right);
+            tail = tail.next;
+            right = right - 1;
+        }
+
+        tail.next = null;
     }
 }
