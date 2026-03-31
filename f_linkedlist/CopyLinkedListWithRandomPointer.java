cc6f26733b148efab6f79aa922804f47e251b1e0
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-03-31 21:49:46 +0530

    Neetcode 150 DSA Practice: Linked List: Copy Linked List with Random Pointer: in-progress v1

--- f_linkedlist/CopyLinkedListWithRandomPointer.java
+++ f_linkedlist/CopyLinkedListWithRandomPointer.java
@@ -1,5 +1,26 @@
 public class CopuLinkedListWithRandomPointer {
     public Node copyRandomList(Node head) {
+        if (head == null) return null;
 
+        Node curr = head;
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node newHead = head.next;
+
+        return newHead;
     }
 }
