948690f23447fab9649f347a7ce573bba7a3f01c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-01 21:35:35 +0530

    Neetcode 150 DSA Practice: Linked List: LRU Cache: in-progress v1

--- f_linkedlist/LRUCache.java
+++ f_linkedlist/LRUCache.java
@@ -10,4 +10,22 @@ public class LRUCache {
             this.value = value;
         }
     }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+    private Node head;
+    private Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<Integer, Node>();
+
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+
 }
