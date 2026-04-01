111d93647da3dcfec4ab9c5050aece3672e8436e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-01 20:34:46 +0530

    Neetcode 150 DSA Practice: Linked List: Add Two Numbers: in-progress v1

--- f_linkedlist/AddTwoNumbers.java
+++ f_linkedlist/AddTwoNumbers.java
@@ -1,5 +1,11 @@
 public class AddTwoNumbes {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
 
+
+
+        return dummy.next;
     }
 }
