8d498c7e291676b22011af708f6bcfd3fb3490c3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-01 20:51:43 +0530

    Neetcode 150 DSA Practice: Linked List: Find the Duplicate Number: in-progress v1

--- f_linkedlist/FindTheDuplicateNumber.java
+++ f_linkedlist/FindTheDuplicateNumber.java
@@ -1,5 +1,14 @@
 public class FindDuplicateNumber {
     ublic int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
 
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+
+        return slow;
     }
 }
