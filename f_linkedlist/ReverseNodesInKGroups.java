9f9149cee2cf43e16b51e6fcf6f3698c63515427
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-02 14:35:04 +0530

    Neetcode 150 DSA Practice: Linked List: ReverseNodesInKGroups: in-progress v1

--- f_linkedlist/ReverseNodesInKGroups.java
+++ f_linkedlist/ReverseNodesInKGroups.java
@@ -1,5 +1,20 @@
 public class ReverseNodesInKGroups {
     public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
+        ListNode groupPrev = dummy;
+
+
+
+        return dummy.next;
+    }
+
+    private ListNode getKth(ListNode curr, int k) {
+        while (curr != null && k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        return curr;
     }
 }
