78cc9cde5e45bc103c7969d8e1085c1f01cb2829
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-02 16:04:01 +0530

    Neetcode 150 DSA Practice: Linked List: DiameterOfBinaryTree: completed

--- g_trees/DiameterOfBinaryTree.java
+++ g_trees/DiameterOfBinaryTree.java
@@ -5,4 +5,15 @@ public class DiameterOfBinaryTree {
         dfs(root);
         return diameter;
     }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return 1 + Math.max(left, right);
+    }
 }
