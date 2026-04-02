4bd7fc331d8872c7ad9bc69cf006ec92b3cef5aa
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-02 11:49:59 +0530

    Neetcode 150 DSA Practice: Linked List: Merge K Sorted Linked Lists: in-progresss v1

--- f_linkedlist/MergeKSortedLinkedLists.java
+++ f_linkedlist/MergeKSortedLinkedLists.java
@@ -1,5 +1,18 @@
 public class MergeKSortedLinkedList {
     public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(
+                (a, b) -> a.val - b.val
+        );
 
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                minHeap.add(lists[i]);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        return dummy.next;
     }
 }
