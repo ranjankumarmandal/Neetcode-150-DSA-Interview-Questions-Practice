50fe0716656f93559ac5a871f830d7ea946c8e2a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-03 15:28:18 +0530

    Neetcode 150 DSA Practice: Trees: SameTree: completed

--- g_trees/SameTree.java
+++ g_trees/SameTree.java
@@ -1,5 +1,7 @@
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
+        f (p == null && q == null) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
