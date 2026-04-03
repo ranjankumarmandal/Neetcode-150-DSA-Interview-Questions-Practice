49957abf2837e4d60681b61229f0c2e6eff0c678
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-03 15:32:21 +0530

    Neetcode 150 DSA Practice: Trees: SubTreeOfBinaryTree: completed

--- g_trees/SubTreeOfBinaryTree.java
+++ g_trees/SubTreeOfBinaryTree.java
@@ -1,5 +1,17 @@
 public class SubTreeOfBinaryTree {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) return true;
+        if (root == null) return false;
 
+        if (isSame(root, subRoot)) return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isSame(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null || a.val != b.val) return false;
+
+        return isSame(a.left, b.left) && isSame(a.right, b.right);
     }
 }
