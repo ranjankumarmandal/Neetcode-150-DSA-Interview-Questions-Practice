acb82a17369f4d8b3496efae5ce0d52efd493116
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-03 15:36:08 +0530

    Neetcode 150 DSA Practice: Trees: LowestCommonAncestorInBST: completed

--- g_trees/LowestCommonAncestorInBST.java
+++ g_trees/LowestCommonAncestorInBST.java
@@ -1,5 +1,14 @@
 public class LowestCommonAncestorInBST {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
     }
 }
