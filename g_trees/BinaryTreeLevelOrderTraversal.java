63e83ed5a0e34e3446629adbedabaeff527f73f7
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-03 16:45:53 +0530

    Neetcode 150 DSA Practice: Trees: BinaryTreeLevelOrderTraversal: completed

--- g_trees/BinaryTreeLevelOrderTraversal.java
+++ g_trees/BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,25 @@
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
 
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = q.size(); i > 0; i--) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+
+            res.add(level);
+        }
+
+        return res;
     }
 }
