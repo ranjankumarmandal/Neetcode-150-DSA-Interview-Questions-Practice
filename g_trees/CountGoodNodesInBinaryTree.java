efb97ea7bbcbcb37eed3746af19ddfac72977214
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-04 20:43:11 +0530

    Neetcode 150 DSA Practice: Trees: CountGoodNodesInBinaryTree: completed

--- g_trees/CountGoodNodesInBinaryTree.java
+++ g_trees/CountGoodNodesInBinaryTree.java
@@ -1,5 +1,17 @@
 public class CountGoodNodesInBinaryTree {
     public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
 
+    private int dfs(TreeNode node, int max) {
+        if (node == null) return 0;
+
+        int res = node.val >= max ? 1 : 0;
+        int newMax = Math.max(max, node.val);
+
+        res += dfs(node.left, newMax);
+        res += dfs(node.right, newMax);
+
+        return res;
     }
 }
