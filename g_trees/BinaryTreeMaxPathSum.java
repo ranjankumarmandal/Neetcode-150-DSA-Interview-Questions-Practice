6e70004f34f5aeb18d23fb406282398ae3ea590d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-04 21:02:34 +0530

    Neetcode 150 DSA Practice: Trees: BinaryTreeMaxPathSum: completed

--- g_trees/BinaryTreeMaxPathSum.java
+++ g_trees/BinaryTreeMaxPathSum.java
@@ -1,5 +1,19 @@
 public class BinaryTreeMaxPathSum {
-    public int maxPathSum(TreeNode root) {
+    int res = Integer.MIN_VALUE;
 
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = Math.max(0, dfs(node.left));
+        int right = Math.max(0, dfs(node.right));
+
+        res = Math.max(res, node.val + left + right);
+
+        return node.val + Math.max(left, right);
     }
 }
