0873bcc75a6e848e2537aadeb5ff1f01ef7243e2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-04 21:26:09 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: KthLargestElementInAStream: completed

--- h_heap_or_priorityqueue/KthLargestElementInAStream.java
+++ h_heap_or_priorityqueue/KthLargestElementInAStream.java
@@ -1,9 +1,23 @@
 public class KthLargestElementInAStream {
-    public KthLargest(int k, int[] nums) {
+    private final PriorityQueue<Integer> minHeap;
+    private final int k;
 
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
     }
 
     public int add(int val) {
-
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
     }
 }
