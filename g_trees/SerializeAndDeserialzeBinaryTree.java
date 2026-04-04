f1d635f87b42e546d3738ece007f26242424525a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-04 21:05:41 +0530

    Neetcode 150 DSA Practice: Trees: SerializeAndDeserialzeBinaryTree: completed

--- g_trees/SerializeAndDeserialzeBinaryTree.java
+++ g_trees/SerializeAndDeserialzeBinaryTree.java
@@ -1,5 +1,36 @@
 public class SerializeAndDeserialzeBinaryTree {
-    public String serialize(TreeNode root) {
+    private int idx;
 
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfsSerialize(root, sb);
+        return sb.toString();
+    }
+
+    private void dfsSerialize(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("N,");
+            return;
+        }
+        sb.append(node.val).append(",");
+        dfsSerialize(node.left, sb);
+        dfsSerialize(node.right, sb);
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] vals = data.split(",");
+        idx = 0;
+        return dfsDeserialize(vals);
+    }
+
+    private TreeNode dfsDeserialize(String[] vals) {
+        if (vals[idx].equals("N")) {
+            idx++;
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(vals[idx++]));
+        node.left = dfsDeserialize(vals);
+        node.right = dfsDeserialize(vals);
+        return node;
     }
 }
