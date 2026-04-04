fe024377b1b505f511de774c50ffed5b0b25ea9d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-04 20:52:41 +0530

    Neetcode 150 DSA Practice: Trees: KthSmallestIntegerInBST: completed

--- g_trees/KthSmallestIntegerInBST.java
+++ g_trees/KthSmallestIntegerInBST.java
@@ -1,5 +1,24 @@
 public class KthSmallestIntegerInBST {
-    public int kthSmallest(TreeNode root, int k) {
+    int k;
+    int res;
 
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        dfs(root);
+        return res;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) return;
+
+        dfs(node.left);
+
+        k--;
+        if (k == 0) {
+            res = node.val;
+            return;
+        }
+
+        dfs(node.right);
     }
 }
