e2ccb921130756b526a9b63bedfab7fd0822026a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 08:41:10 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: FindMedianFromDataStream: completed

--- h_heap_or_priorityqueue/FindMedianFromDataStream.java
+++ h_heap_or_priorityqueue/FindMedianFromDataStream.java
@@ -1,13 +1,33 @@
 public class FindMedianFromDataStream {
-    public MedianFinder() {
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> large;
 
+    public MedianFinder() {
+        small = new PriorityQueue<>((a, b) -> b - a);
+        large = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
+        if (!large.isEmpty() && num > large.peek()) {
+            large.offer(num);
+        } else {
+            small.offer(num);
+        }
 
+        if (small.size() > large.size() + 1) {
+            large.offer(small.poll());
+        } else if (large.size() > small.size() + 1) {
+            small.offer(large.poll());
+        }
     }
 
     public double findMedian() {
-
+        if (small.size() > large.size()) {
+            return small.peek();
+        } else if (large.size() > small.size()) {
+            return large.peek();
+        } else {
+            return (small.peek() + large.peek()) / 2.0;
+        }
     }
 }
