96fb2575ef90df619dfccbc74316ccb1019b27f4
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 07:44:22 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: KthLargestElementInAnArray: completed

--- h_heap_or_priorityqueue/KthLargestElementInAnArray.java
+++ h_heap_or_priorityqueue/KthLargestElementInAnArray.java
@@ -1,5 +1,15 @@
 public class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
     }
+
 }
