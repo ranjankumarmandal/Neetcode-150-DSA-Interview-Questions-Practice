ecf65174ba303e8d2b5e19898c1c4f38346d1542
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 07:49:07 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: TaskScheduler: completed

--- h_heap_or_priorityqueue/TaskScheduler.java
+++ h_heap_or_priorityqueue/TaskScheduler.java
@@ -1,5 +1,23 @@
 public class TaskScheduler {
     public int leastInterval(char[] tasks, int n) {
+        int[] count = new int[26];
 
+        for (char t : tasks) {
+            count[t - 'A']++;
+        }
+
+        int maxf = 0;
+        for (int c : count) {
+            maxf = Math.max(maxf, c);
+        }
+
+        int maxCount = 0;
+        for (int c : count) {
+            if (c == maxf) maxCount++;
+        }
+
+        int time = (maxf - 1) * (n + 1) + maxCount;
+
+        return Math.max(tasks.length, time);
     }
 }
