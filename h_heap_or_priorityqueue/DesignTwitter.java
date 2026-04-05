0d75cb7c265215ef9b659f7f293a05a22b2ff97f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 08:33:53 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: DesignTwitter: in-progresss

--- h_heap_or_priorityqueue/DesignTwitter.java
+++ h_heap_or_priorityqueue/DesignTwitter.java
@@ -1,3 +1,21 @@
 public class DesignTwitter {
+    public Twitter() {
 
+    }
+
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+
+    }
+
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+
+    }
 }
