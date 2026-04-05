0d3acd4c5a42bbf3f938c1171cb0fb506aaf37fb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 07:40:52 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: KClosestPointsToOrigin: completed

--- h_heap_or_priorityqueue/KClosestPointsToOrigin.java
+++ h_heap_or_priorityqueue/KClosestPointsToOrigin.java
@@ -1,5 +1,21 @@
 public class KClosestPointsToOrigin {
     public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])
+        );
 
+        for (int[] p : points) {
+            maxHeap.offer(p);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            res[i] = maxHeap.poll();
+        }
+
+        return res;
     }
 }
