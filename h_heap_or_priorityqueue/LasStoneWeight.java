987d1fdfaa25a61c15f6d7d4de49bf3618423df5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 07:35:57 +0530

    Neetcode 150 DSA Practice: Heap Or Priority Queue: LasStoneWeight: completed

--- h_heap_or_priorityqueue/LasStoneWeight.java
+++ h_heap_or_priorityqueue/LasStoneWeight.java
@@ -1,5 +1,20 @@
 public class LasStoneWeight {
     public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
 
+        for (int s : stones) {
+            maxHeap.offer(s);
+        }
+
+        while (maxHeap.size() > 1) {
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+
+            if (y != x) {
+                maxHeap.offer(y - x);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
     }
 }
