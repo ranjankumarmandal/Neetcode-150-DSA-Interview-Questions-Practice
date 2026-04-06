1ab57efb5a43fd705281684c72d25c1700f36d86
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-06 16:31:23 +0530

    Neetcode 150 DSA Practice: Backtracking: Permutations: completed

--- i_backtracking/Permutations.java
+++ i_backtracking/Permutations.java
@@ -1,5 +1,28 @@
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, res);
+        return res;
+    }
 
+    private void backtrack(int[] nums, int index, List<List<Integer>> res) {
+        if (index == nums.length) {
+            List<Integer> perm = new ArrayList<>();
+            for (int num : nums) perm.add(num);
+            res.add(perm);
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            backtrack(nums, index + 1, res);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
