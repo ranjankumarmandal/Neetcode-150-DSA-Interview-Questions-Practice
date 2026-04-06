b73505a19f9da2960c9a994faac15890f423079c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-06 16:37:04 +0530

    Neetcode 150 DSA Practice: Backtracking: Subsets2: completed

--- i_backtracking/Subsets2.java
+++ i_backtracking/Subsets2.java
@@ -1,5 +1,22 @@
 public class Subsets2 {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
 
+    private void backtrack(int[] nums, int start,
+                           List<Integer> subset,
+                           List<List<Integer>> res) {
+        res.add(new ArrayList<>(subset));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            subset.add(nums[i]);
+            backtrack(nums, i + 1, subset, res);
+            subset.remove(subset.size() - 1);
+        }
     }
 }
