62e5f9b9c21567d57b54808600ab0de855222214
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-06 16:54:19 +0530

    Neetcode 150 DSA Practice: Backtracking: WordSearch: completed

--- i_backtracking/WordSearch.java
+++ i_backtracking/WordSearch.java
@@ -1,5 +1,30 @@
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
+        int rows = board.length, cols = board[0].length;
 
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (dfs(board, word, r, c, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) return true;
+
+        if (r < 0 || c < 0 || r >= board.length || c >= board[0].length ||
+                board[r][c] != word.charAt(i)) return false;
+
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        boolean found = dfs(board, word, r + 1, c, i + 1) ||
+                dfs(board, word, r - 1, c, i + 1) ||
+                dfs(board, word, r, c + 1, i + 1) ||
+                dfs(board, word, r, c - 1, i + 1);
+
+        board[r][c] = temp;
+        return found;
     }
 }
