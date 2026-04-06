2d076b16463496dd3317398663141933d6f80b41
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-06 16:46:21 +0530

    Neetcode 150 DSA Practice: Backtracking: GenerateParenthesis: completed

--- i_backtracking/GenerateParenthesis.java
+++ i_backtracking/GenerateParenthesis.java
@@ -1,5 +1,28 @@
 public class GenerateParenthesis {
     public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(0, 0, n, new StringBuilder(), res);
+        return res;
+    }
 
+    private void backtrack(int open, int close, int n,
+                           StringBuilder curr,
+                           List<String> res) {
+        if (open == n && close == n) {
+            res.add(curr.toString());
+            return;
+        }
+
+        if (open < n) {
+            curr.append('(');
+            backtrack(open + 1, close, n, curr, res);
+            curr.deleteCharAt(curr.length() - 1);
+        }
+
+        if (close < open) {
+            curr.append(')');
+            backtrack(open, close + 1, n, curr, res);
+            curr.deleteCharAt(curr.length() - 1);
+        }
     }
 }
