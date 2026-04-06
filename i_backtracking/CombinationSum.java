3418e13c4f9a7f2ab46e1852eb75318712ee1c4b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-06 15:59:46 +0530

    Neetcode 150 DSA Practice: Backtracking: CombinationSum: completed

--- i_backtracking/CombinationSum.java
+++ i_backtracking/CombinationSum.java
@@ -1,5 +1,25 @@
 public class CombinationSum {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    ublic List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, target, 0, new ArrayList<>(), result);
+        return result;
+    }
 
+    private void backtrack(int[] nums, int target, int start,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if (nums[i] > target) break;
+
+            current.add(nums[i]);
+            backtrack(nums, target - nums[i], i, current, result);
+            current.remove(current.size() - 1);
+        }
     }
 }
