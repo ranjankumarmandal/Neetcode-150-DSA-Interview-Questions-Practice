f6f549ccb4eb540a6e1e7c4ec3e32cf616cb5b2f
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-07 23:06:42 +0530

    Neetcode 150 DSA Practice: Trie: ImplementTrie: inprogress v5

--- j_tries/ImplementTrie.java
+++ j_tries/ImplementTrie.java
@@ -10,4 +10,13 @@ public class ImplementTrie {
         root = new Node();
     }
 
+    public void insert(String word) {
+        Node cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) cur.children[i] = new Node();
+            cur = cur.children[i];
+        }
+        cur.end = true;
+    }
 }
