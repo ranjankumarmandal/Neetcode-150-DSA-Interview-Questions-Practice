6592b13dea19e5d83d542e97ddf97a72c11bfae2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-07 22:58:08 +0530

    Neetcode 150 DSA Practice: Backtracking: NQueens: in-progress v2

--- i_backtracking/NQueens.java
+++ i_backtracking/NQueens.java
@@ -4,6 +4,9 @@ public class NQueens {
         char[][] board = new char[n][n];
         for (char[] row : board) Arrays.fill(row, '.');
 
+        boolean[] col = new boolean[n];
+        boolean[] posDiag = new boolean[2 * n];
+        boolean[] negDiag = new boolean[2 * n];
 
         backtrack(0, n, board, col, posDiag, negDiag, res);
         return res;
