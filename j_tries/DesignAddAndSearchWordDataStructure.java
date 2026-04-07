3ee07827c3fc3a8c8e5a176f1fe44c168ae3818c
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-07 23:12:58 +0530

    Neetcode 150 DSA Practice: Trie: DesignAddAndSearchWordDataStructure: in-progress v4

--- j_tries/DesignAddAndSearchWordDataStructure.java
+++ j_tries/DesignAddAndSearchWordDataStructure.java
@@ -9,4 +9,19 @@ public class DesignAddAndSearchWordDataStructure {
     public WordDictionary() {
         root = new Node();
     }
+
+    public void addWord(String word) {
+        Node cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) cur.children[i] = new Node();
+            cur = cur.children[i];
+        }
+        cur.end = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
 }
