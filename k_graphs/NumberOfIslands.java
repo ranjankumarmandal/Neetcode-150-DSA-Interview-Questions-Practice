0b5f6aa4290c09f1b40996ba7a2e70af0d30a5b3
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-09 17:27:07 +0530

    Neetcode 150 DSA Practice: Graphs: NumberOfIslands: in-progress: completed numIsland()

--- k_graphs/NumberOfIslands.java
+++ k_graphs/NumberOfIslands.java
@@ -2,7 +2,14 @@ public class NumberOfIslands {
     public int numIslands(char[][] grid) {
         int m = grid.length, n = grid[0].length, count = 0;
 
-
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
 
         return count;
     }
