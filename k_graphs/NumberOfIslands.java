1ffe1a29b8819be316c7a1111e04319111203f86
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-09 17:28:24 +0530

    Neetcode 150 DSA Practice: Graphs: NumberOfIslands: in-progress: started working for dfs() v1

--- k_graphs/NumberOfIslands.java
+++ k_graphs/NumberOfIslands.java
@@ -15,6 +15,7 @@ public class NumberOfIslands {
     }
 
     private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
 
     }
 }
