b76ea9e5cb7d5ebc64b89fcf4c0c79b797eb80f6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-10 08:19:44 +0530

    Neetcode 150 DSA Practice: Graphs: CloneGraph: in-progress: created cloneGraph()

--- k_graphs/CloneGraph.java
+++ k_graphs/CloneGraph.java
@@ -1,3 +1,8 @@
 public class CloneGraph {
     private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        return dfs(node);
+    }
 }
