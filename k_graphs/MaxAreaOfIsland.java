53327836d32bc03828dabe04f422491a2fa5c8f8
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-10 08:10:00 +0530

    Neetcode 150 DSA Practice: Graphs: MaxAreaOfIsland: in-progress v1

--- k_graphs/MaxAreaOfIsland.java
+++ k_graphs/MaxAreaOfIsland.java
@@ -1,5 +1,15 @@
 public class MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int max = 0;
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, dfs(grid, i, j, m, n));
+                }
+            }
+        }
+        return max;
     }
 }
