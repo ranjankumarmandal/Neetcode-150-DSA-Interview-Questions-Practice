b0a9c27a1241667e24b7c4319422fd8e9501db75
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-10 08:10:29 +0530

    Neetcode 150 DSA Practice: Graphs: MaxAreaOfIsland: completed

--- k_graphs/MaxAreaOfIsland.java
+++ k_graphs/MaxAreaOfIsland.java
@@ -12,4 +12,16 @@ public class MaxAreaOfIsland {
         }
         return max;
     }
+
+    private int dfs(int[][] grid, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) return 0;
+
+        grid[i][j] = 0;
+
+        return 1
+                + dfs(grid, i + 1, j, m, n)
+                + dfs(grid, i - 1, j, m, n)
+                + dfs(grid, i, j + 1, m, n)
+                + dfs(grid, i, j - 1, m, n);
+    }
 }
