ffa2b763fd5e714b46487763d8a52486562907b2
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-11 16:46:14 +0530

    Neetcode 150 DSA Practice: Graphs: IslandsAndTreasure: in-progress v1

--- k_graphs/IslandsAndTreasure.java
+++ k_graphs/IslandsAndTreasure.java
@@ -1,5 +1,19 @@
 public class IslandsAndTreasure {
     public void islandsAndTreasure(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 0) {
+                    q.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
 
     }
 }
