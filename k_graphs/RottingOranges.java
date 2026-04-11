29d6ab9afe88816d61b6c9679d77a042ff8c4608
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-11 16:51:47 +0530

    Neetcode 150 DSA Practice: Graphs: RottingOranges: completed

--- k_graphs/RottingOranges.java
+++ k_graphs/RottingOranges.java
@@ -16,6 +16,29 @@ public class RottingOranges {
 
         int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
+        while (fresh > 0 && !q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+                int r = cur[0], c = cur[1];
+
+                for (int[] d : dirs) {
+                    int nr = r + d[0];
+                    int nc = c + d[1];
+
+                    if (nr < 0 || nc < 0 || nr >= rows || nc >= cols || grid[nr][nc] != 1) {
+                        continue;
+                    }
+
+                    grid[nr][nc] = 2;
+                    fresh--;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+
+            time++;
+        }
 
         return fresh == 0 ? time : -1;
     }
