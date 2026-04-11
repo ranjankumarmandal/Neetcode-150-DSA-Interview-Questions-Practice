c729c112421a0ec2fbbec932d65bcae5d82f10f5
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-11 16:55:43 +0530

    Neetcode 150 DSA Practice: Graphs: PacificAtlanticWaterFlow: in-progress v1

--- k_graphs/PacificAtlanticWaterFlow.java
+++ k_graphs/PacificAtlanticWaterFlow.java
@@ -1,5 +1,34 @@
 public class PacificAtlanticWaterFlow {
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    int rows, cols;
 
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        rows = heights.length;
+        cols = heights[0].length;
+
+        boolean[][] pac = new boolean[rows][cols];
+        boolean[][] atl = new boolean[rows][cols];
+
+        for (int c = 0; c < cols; c++) {
+            dfs(0, c, pac, heights);
+            dfs(rows - 1, c, atl, heights);
+        }
+
+        for (int r = 0; r < rows; r++) {
+            dfs(r, 0, pac, heights);
+            dfs(r, cols - 1, atl, heights);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (pac[r][c] && atl[r][c]) {
+                    res.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return res;
     }
 }
