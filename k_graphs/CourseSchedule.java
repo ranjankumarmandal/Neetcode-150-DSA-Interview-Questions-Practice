e0372a21e9653a116ecf7e473ca4f03c6a4bee1b
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-12 11:56:31 +0530

    Neetcode 150 DSA Practice: Graphs: CourseSchedule: in-progress v1

--- k_graphs/CourseSchedule.java
+++ k_graphs/CourseSchedule.java
@@ -1,5 +1,21 @@
 public class CourseSchedule {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> graph;
+    int[] state;
 
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
+
+        for (int[] p : prerequisites) {
+            graph.get(p[0]).add(p[1]);
+        }
+
+        state = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i)) return false;
+        }
+
+        return true;
     }
 }
