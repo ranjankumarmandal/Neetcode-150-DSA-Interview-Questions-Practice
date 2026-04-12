af816ae0b7a0b52ff2ef2d432241b74341cd9e5a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-12 11:59:29 +0530

    Neetcode 150 DSA Practice: Graphs: CourseSchedule2: in-progress v1

--- k_graphs/CourseSchedule2.java
+++ k_graphs/CourseSchedule2.java
@@ -1,5 +1,20 @@
 public class CourseSchedule2 {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
+        int[] indegree = new int[numCourses];
+
+        for (int[] p : prerequisites) {
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.offer(i);
+        }
+
 
     }
 }
