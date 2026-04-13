bcae13324a9446f75d022c8364bc2a1bc5cf1884
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-13 21:21:34 +0530

    Neetcode 150 DSA Practice: Graphs: GraphValidTree: in-progress v1

--- k_graphs/GraphValidTree.java
+++ k_graphs/GraphValidTree.java
@@ -1,5 +1,10 @@
 public class GraphValidTree {
     public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1) return false;
 
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+
+        return true;
     }
 }
