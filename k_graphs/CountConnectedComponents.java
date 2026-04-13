9b9735f13104f69eb0fab07c2b5ef82b8248e601
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-13 21:25:58 +0530

    Neetcode 150 DSA Practice: Graphs: CountConnectedComponents: in-progress v

--- k_graphs/CountConnectedComponents.java
+++ k_graphs/CountConnectedComponents.java
@@ -1,5 +1,15 @@
 public class CountConnectedComponents {
     public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        int[] rank = new int[n];
 
+
+        for (int[] e : edges) {
+            if (union(e[0], e[1], parent, rank)) {
+                components--;
+            }
+        }
+
+        return components;
     }
 }
