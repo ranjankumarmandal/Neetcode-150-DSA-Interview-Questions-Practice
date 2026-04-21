3a4a46dc2f134594288f75786e61ca3484e72d04
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-21 22:48:27 +0530

    Neetcode 150 dsa: RedundantConnection: in-progress v1

--- k_graphs/RedundantConnection.java
+++ k_graphs/RedundantConnection.java
@@ -1,5 +1,13 @@
 public class RedundantConnection {
-    public int[] findRedundantConnection(int[][] edges) {
+    int[] parent;
 
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent = new int[n + 1];
+
+
+
+        return new int[0];
     }
+
 }
