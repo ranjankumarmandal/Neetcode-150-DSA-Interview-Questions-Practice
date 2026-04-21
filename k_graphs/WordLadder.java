3e7d99f185bcc7f93e805bfe92ce595a6196cc0d
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-21 22:52:20 +0530

    Neetcode 150 dsa: WordLadder: in-progress v1

--- k_graphs/WordLadder.java
+++ k_graphs/WordLadder.java
@@ -1,5 +1,30 @@
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    ublic int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
 
+        if (!set.contains(endWord)) return 0;
+
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+
+        int level = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                String word = q.poll();
+
+                if (word.equals(endWord)) return level;
+
+                char[] arr = word.toCharArray();
+
+
+            }
+
+            level++;
+        }
+
+        return 0;
     }
 }
