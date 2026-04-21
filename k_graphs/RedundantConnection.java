02ba88758cde89d8b3ffddc3f24447d38b2e7c09
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-21 22:49:15 +0530

    Neetcode 150 dsa: RedundantConnection: complete

--- k_graphs/RedundantConnection.java
+++ k_graphs/RedundantConnection.java
@@ -5,7 +5,19 @@ public class RedundantConnection {
         int n = edges.length;
         parent = new int[n + 1];
 
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
 
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+
+            if (find(u) == find(v)) {
+                return e;
+            }
+
+            union(u, v);
+        }
 
         return new int[0];
     }
