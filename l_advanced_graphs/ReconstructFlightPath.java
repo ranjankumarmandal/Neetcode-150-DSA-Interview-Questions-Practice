e3c54d74be4649ebba12413a8ebbad55cb30d7d9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-22 23:18:01 +0530

    Neetcode 150 dsa: Advanced Graphs: ReconstructFlightPath: completed

--- l_advanced_graphs/ReconstructFlightPath.java
+++ l_advanced_graphs/ReconstructFlightPath.java
@@ -1,5 +1,22 @@
 public class ReconstructFlightPath {
-    ublic List<String> findItinerary(List<List<String>> tickets) {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> adj = new HashMap<>();
 
+        for (List<String> t : tickets) {
+            adj.computeIfAbsent(t.get(0), k -> new PriorityQueue<>()).offer(t.get(1));
+        }
+
+        LinkedList<String> res = new LinkedList<>();
+        dfs("JFK", adj, res);
+        return res;
     }
+
+    private void dfs(String src, Map<String, PriorityQueue<String>> adj, LinkedList<String> res) {
+        PriorityQueue<String> pq = adj.get(src);
+        while (pq != null && !pq.isEmpty()) {
+            dfs(pq.poll(), adj, res);
+        }
+        res.addFirst(src);
+    }
+
 }
