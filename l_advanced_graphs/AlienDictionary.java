e50d21431f9b2806e0940d03317e2f06238a7ccb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-23 06:42:54 +0530

    Neetcode 150 dsa: Advanced Graphs: AlienDictionary: in-progress v1

--- l_advanced_graphs/AlienDictionary.java
+++ l_advanced_graphs/AlienDictionary.java
@@ -1,5 +1,41 @@
 public class AlienDictionary {
     public String foreignDictionary(String[] words) {
+        Map<Character, Set<Character>> adj = new HashMap<>();
+        Map<Character, Integer> indegree = new HashMap<>();
 
+        for (String w : words) {
+            for (char c : w.toCharArray()) {
+                adj.putIfAbsent(c, new HashSet<>());
+                indegree.putIfAbsent(c, 0);
+            }
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            String w1 = words[i], w2 = words[i + 1];
+            int min = Math.min(w1.length(), w2.length());
+
+            if (w1.length() > w2.length() && w1.substring(0, min).equals(w2.substring(0, min)))
+                return "";
+
+            for (int j = 0; j < min; j++) {
+                char c1 = w1.charAt(j), c2 = w2.charAt(j);
+                if (c1 != c2) {
+                    if (!adj.get(c1).contains(c2)) {
+                        adj.get(c1).add(c2);
+                        indegree.put(c2, indegree.get(c2) + 1);
+                    }
+                    break;
+                }
+            }
+        }
+
+        Queue<Character> q = new LinkedList<>();
+        for (char c : indegree.keySet()) {
+            if (indegree.get(c) == 0) q.offer(c);
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        return res.length() == indegree.size() ? res.toString() : "";
     }
 }
