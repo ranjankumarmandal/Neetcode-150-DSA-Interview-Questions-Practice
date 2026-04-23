8f5a9f869b78c93a914065e48b7f480cc197384e
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-23 06:46:33 +0530

    Neetcode 150 dsa: Advanced Graphs: CheapestFlightsWithinKStops: in-progress v1

--- l_advanced_graphs/CheapestFlightsWithinKStops.java
+++ l_advanced_graphs/CheapestFlightsWithinKStops.java
@@ -1,5 +1,10 @@
 public class CheapestFlightsWithinKStops {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
 
+
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
     }
 }
