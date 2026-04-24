1551fcb4c82a8b879b6157ed119432d4fb2c70ae
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-24 23:48:50 +0530

    Neetcode 150 dsa: DP: Dynamic Programming: MinCostClimbingStairs: completed

--- m_1D_DP_dynamic_programming/MinCostClimbingStairs.java
+++ m_1D_DP_dynamic_programming/MinCostClimbingStairs.java
@@ -1,5 +1,18 @@
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
 
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i <= n; i++) {
+            int fromOneStep = dp[i - 1] + cost[i - 1];
+            int fromTwoStep = dp[i - 2] + cost[i - 2];
+
+            dp[i] = Math.min(fromOneStep, fromTwoStep);
+        }
+
+        return dp[n];
     }
 }
