21578c83cc589e6cafb8a5fc9f67bb23c184be78
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-24 23:52:21 +0530

    Neetcode 150 dsa: DP: Dynamic Programming: HouseRobber2: completed

--- m_1D_DP_dynamic_programming/HouseRobber2.java
+++ m_1D_DP_dynamic_programming/HouseRobber2.java
@@ -1,5 +1,17 @@
 public class HouseRobber2 {
     public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(helper(nums, 0, n - 2), helper(nums, 1, n - 1));
+    }
 
+    private int helper(int[] nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(prev1 + nums[i], prev2);
+            prev1 = prev2;
+            prev2 = temp;
+        }
+        return prev2;
     }
 }
