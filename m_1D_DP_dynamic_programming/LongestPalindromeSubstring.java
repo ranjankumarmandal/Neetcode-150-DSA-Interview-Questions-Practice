1f7dfee488ba76b3dc9830f6521902027c275332
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-26 23:55:50 +0530

    Neetcode 150 dsa: DP: Dynamic Programming: LongestPalindromeSubstring: in-progress v1

--- m_1D_DP_dynamic_programming/LongestPalindromeSubstring.java
+++ m_1D_DP_dynamic_programming/LongestPalindromeSubstring.java
@@ -1,5 +1,16 @@
 public class LongestPalindromeSubstring {
     public String longestPalindrome(String s) {
+        int start = 0, end = 0;
 
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 }
