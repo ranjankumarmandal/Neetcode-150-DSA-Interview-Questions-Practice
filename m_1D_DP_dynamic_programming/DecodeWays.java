3854cd68061584eb7324f3c5c136c55af8e6bc08
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-26 23:59:25 +0530

    Neetcode 150 dsa: DP: Dynamic Programming: DecodeWays: in-progress v1

--- m_1D_DP_dynamic_programming/DecodeWays.java
+++ m_1D_DP_dynamic_programming/DecodeWays.java
@@ -1,5 +1,21 @@
 public class DecodeWays {
     public int numDecodings(String s) {
+        int n = s.length();
+        int next1 = 1, next2 = 0;
 
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = 0;
+
+            if (s.charAt(i) != '0') {
+                curr = next1;
+
+                if (i < n - 1 && (s.charAt(i) == '1' ||
+                        (s.charAt(i) == '2' && s.charAt(i + 1) <= '6'))) {
+                    curr += next2;
+                }
+            }
+        }
+
+        return next1;
     }
 }
