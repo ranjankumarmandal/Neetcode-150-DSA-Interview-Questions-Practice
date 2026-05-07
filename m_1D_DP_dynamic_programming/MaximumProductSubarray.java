c5af3f555343f492acef20f6cb4cb232d1df35e9
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-05-07 19:50:18 +0530

    Neetcode 150 dsa: DP: Dynamic Programming: MaximumProductSubarray: completed

--- m_1D_DP_dynamic_programming/MaximumProductSubarray.java
+++ m_1D_DP_dynamic_programming/MaximumProductSubarray.java
@@ -1,5 +1,18 @@
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
+        int res = nums[0];
+        int curMax = 1;
+        int curMin = 1;
 
+        for (int num : nums) {
+            int temp = curMax * num;
+
+            curMax = Math.max(num, Math.max(curMax * num, curMin * num));
+            curMin = Math.min(num, Math.min(temp, curMin * num));
+
+            res = Math.max(res, curMax);
+        }
+
+        return res;
     }
 }
